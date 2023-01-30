7b775d3185f8d29a4c74c775d6b7e2b1d5eb407b
Lycolia Rizzim <lycolia@lycolia.info>
2023-01-31 00:31:42 +0900

    refactor: rename variables

--- src/libs/Acme/index.ts
+++ src/libs/Acme/index.ts
@@ -1,9 +1,9 @@
 const createAcmeDomain = (rootDomain: string, targetDomain: string) => {
-  const regdRootDomain = rootDomain.replace(/\./g, '\\.');
-  const re = new RegExp(`^(.+?)\\.${regdRootDomain}`);
-  const domainMat = targetDomain.match(re);
-  return domainMat !== null
-    ? `_acme-challenge.${domainMat[1]}` // for sub domain
+  const escapedRootDomain = rootDomain.replace(/\./g, '\\.');
+  const reAcmeMatcher = new RegExp(`^(.+?)\\.${escapedRootDomain}`);
+  const subDomainCapture = targetDomain.match(reAcmeMatcher);
+  return subDomainCapture !== null
+    ? `_acme-challenge.${subDomainCapture[1]}` // for sub domain
     : '_acme-challenge'; // for root domain
 };
 
