8eb1f98c7ddd13dc6ffd38b76327b0dbc397aa4b
Lycolia Rizzim <lycolia@lycolia.info>
2024-05-08 09:50:32 +0900

    jestのパス設定周りを見直し

--- jest.config.js
+++ jest.config.js
@@ -5,19 +5,17 @@ module.exports = {
   coverageProvider: 'v8',
   testEnvironment: 'node',
   roots: ['<rootDir>/src/'],
-  moduleNameMapper: {
-    'src/(.*)$': '<rootDir>/src/$1'
-  },
   moduleFileExtensions: ['ts', 'js'],
   collectCoverageFrom: ['src/**/*.{ts,js}'],
-  testPathIgnorePatterns: ['<rootDir>[/\\\\](node_modules|dist)[/\\\\]'],
+  coveragePathIgnorePatterns: ['src/types'],
+  testPathIgnorePatterns: ['(node_modules|dist)'],
   transform: {
     '^.+\\.ts$': [
       '@swc/jest',
       {
         sourceMaps: true,
         module: {
-          type: 'commonjs'
+          type: 'commonjs',
         },
         jsc: {
           target: 'es2020',
@@ -25,19 +23,19 @@ module.exports = {
             syntax: 'typescript',
             tsx: false,
             decorators: false,
-            dynamicImport: false
+            dynamicImport: false,
           },
           baseUrl: '.',
           paths: {
             'src/*': ['src/*'],
-            'package.json': ['package.json']
-          }
-        }
-      }
-    ]
+            'package.json': ['package.json'],
+          },
+        },
+      },
+    ],
   },
   watchPlugins: [
     'jest-watch-typeahead/filename',
-    'jest-watch-typeahead/testname'
-  ]
+    'jest-watch-typeahead/testname',
+  ],
 };
