c854ab8d0553c7912dcf31ca4579bc43e45191fb
Lycolia Rizzim <lycolia@lycolia.info>
2026-02-24 17:12:03 +0900

    起動時に非推奨メッセージを出すように変更

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,17 @@ import { MyError } from './libs/MyError';
 import { createOrUpdateRecords } from './libs/RecordManipulator';
 import {
   requestGetDnsConf,
-  requestUpdateDnsConf,
+  requestUpdateDnsConf
 } from './libs/ValueDomainClient';
 import { UnknownError } from './resources/ErrorDefines';
 import pkg from '../package.json';
 
 const main = async () => {
+  Logger.warn('本プログラムは非推奨となりました。');
+  Logger.warn(
+    'このリポジトリで開発していたものは以下に引き継いでいますので、こちらをご利用ください。'
+  );
+  Logger.warn('https://github.com/Lycolia/value-domain-dns-util');
   Logger.info(`${pkg.name} ${pkg.version}`);
   const args = getArguments(process.argv);
   const acme = createAcmeContext(args.rootDomain, process.env);
@@ -28,7 +33,7 @@ const main = async () => {
     domainid: dnsConf.domainid,
     ns_type: dnsConf.ns_type,
     records: newRecords,
-    ttl: dnsConf.ttl,
+    ttl: dnsConf.ttl
   });
 };
 
